55e619d050bdf70f2fb419a28a6d37c423c5b5bb
Praseed Shrestha <p.stha@hotmail.com>
2021-12-21 15:25:45 +0545

    Add project files.

---
+++ FLStore.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FLStore.Web.Startup))]
+namespace FLStore.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
